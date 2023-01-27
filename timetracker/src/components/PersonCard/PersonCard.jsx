fc6b6a1e51b7b888c90a78929689890d0f59e5ce
Steven <scroft001@gmail.com>
2023-01-26 17:23:54 -0800

    Person Card base scss done

--- timetracker/src/components/PersonCard/PersonCard.jsx
+++ timetracker/src/components/PersonCard/PersonCard.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import jeremy from '../../images/image-jeremy.png'
+import './PersonCard.scss'
 
 const PersonCard = () => {
   return (
-    <div>PersonCard</div>
+    <section className='app__personCard'>
+        <div className='app__personCard-profile'>
+            <img className='app__personCard-profile-img' src={jeremy} alt='' />
+            <p>Report for</p>
+            <h1>Jeremy Robson</h1>
+        </div>
+        <div className='app__personCard-timeframe'>
+            <p className='time'>Daily</p>
+            <p className='time active'>Weekly</p>
+            <p className='time'>Monthly</p>
+        </div>
+    </section>
   )
 }
 
