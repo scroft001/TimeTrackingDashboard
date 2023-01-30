ff9f1eef0bf5a75abf0c9ec0c79662c836f9e6d1
Steven <scroft001@gmail.com>
2023-01-30 15:32:35 -0800

    Corrected props for tracker cards

--- timetracker/src/components/Dashboard/Dashboard.jsx
+++ timetracker/src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { PersonCard } from '..'
+import { PersonCard, Tracker } from '..'
+import './Dashboard.scss'
 
 const Dashboard = () => {
   return (
     <div className='app__dashboard'>
       <PersonCard />
-      {/* loop through tracker cards */}
+      <div className='app__dashboard-tasks'>
+        <Tracker title="Work" time="32" pastHours="36" />
+        <Tracker title="Play" time="10" pastHours="8" />
+        <Tracker title="Study" time="4" pastHours="7" />
+        <Tracker title="Exercise" time="4" pastHours="5" />
+        <Tracker title="Social" time="5" pastHours="10" />
+        <Tracker title="Self Care" time="2" pastHours="2" />
+      </div>
     </div>
   )
 }
--- timetracker/src/components/Tracker/Tracker.jsx
+++ timetracker/src/components/Tracker/Tracker.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './Tracker.scss'
 
-const Tracker = () => {
+const Tracker = (props) => {
   return (
-    <div>Tracker</div>
+    <div className='app__tracker'>
+      <div className='app__tracker-stats'>
+        <div className='title'>
+          <h2>{props.title}</h2>
+          <p>...</p>
+        </div>
+        <div className='stats'>
+          <p className='hours'>{props.time}hrs</p>
+          <p className='past-hours'>Last Week - {props.pastHours}hrs</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
